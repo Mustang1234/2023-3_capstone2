55baf79795a0033a1ab3fba6dab2da4595402906
32gurihs@gmail.com <32gurihs@gmail.com>
2023-11-22 21:44:23 +0900

    5

--- Nodejs-master/tester.js
+++ Nodejs-master/tester.js
@@ -9,41 +9,47 @@ const ip = '20.39.186.138';
 app.use(bodyParser.json());
 
 // 첫 번째 POST 요청 처리
-app.post('/login', (req, res) => {
-    const requestData = req.body;
-    
-    console.log('Received data from first POST request:', requestData);
-    
-    const secondPostData = {
-        Student_id: requestData.Student_id,
-        Student_pw: requestData.Student_pw
-      };
+app.post('/login', async (req, res) => {
+    try {
+        const requestData = req.body;
+        console.log('Received data from first POST request:', requestData);
 
-    var data;
-    fetch(`http://${ip}:${port}/login_process`, {
-        method: 'POST',
-        body: JSON.stringify(secondPostData),
-    })
-        .then(response => response.json())
-        .then(_data => {
-            data = _data;
-            console.log('Response from second POST request:', data);
-        })
-        .catch(error => {
-            console.error('Error during second POST request:', error);
+        const secondPostData = {
+            Student_id: requestData.Student_id,
+            Student_pw: requestData.Student_pw
+        };
+
+        // 두 번째 POST 요청을 보내고 응답을 기다림
+        const response = await fetch(`http://${ip}:${port}/login_process`, {
+            method: 'POST',
+            body: JSON.stringify(secondPostData),
+            headers: {
+                'Content-Type': 'application/json'
+            },
         });
 
-    res.json({ result: 'success', result: data });
+        // 응답이 JSON 형식인지 확인
+        if (!response.ok) {
+            throw new Error('Failed to fetch');
+        }
+
+        const data = await response.json();
+        console.log('Response from second POST request:', data);
+
+        // 첫 번째 POST 요청에 응답
+        res.json({ result: 'success', data });
+    } catch (error) {
+        console.error('Error during first POST request:', error);
+        res.status(500).json({ result: 'error', error: error.message });
+    }
 });
 
 app.post('/login_process', (req, res) => {
     const requestData = req.body;
-
     console.log('Received data from second POST request:', requestData);
-    const { Student_id, Student_pw } = req.body;
-    const secondPostData = { Student_id: Student_id, Student_pw: Student_pw };
 
-    res.json({ result: 'success', result: secondPostData });
+    // 두 번째 POST 요청에 응답
+    res.json({ result: 'success', data: requestData });
 });
 
 // 서버 시작
