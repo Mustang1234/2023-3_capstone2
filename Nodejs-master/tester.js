961adfb1cccf5942bec2a9174b4338434489f955
32gurihs@gmail.com <32gurihs@gmail.com>
2023-11-22 21:20:01 +0900

    5

--- Nodejs-master/tester.js
+++ Nodejs-master/tester.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
-const port = 3000;
+const port = 1234;
 
 // body-parser middleware 설정
 app.use(bodyParser.json());
