122c39bdef3911ecdf5d5e41655e611f34145c7d
32gurihs@gmail.com <32gurihs@gmail.com>
2023-11-23 14:15:03 +0900

    5

--- Nodejs-master/tester2.js
+++ Nodejs-master/tester2.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
 const jwt = require('jsonwebtoken');
