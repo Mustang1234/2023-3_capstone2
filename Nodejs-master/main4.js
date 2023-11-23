4ff80871b93c06e9a4f9140d7aaf7a9a4bc47c03
32gurihs@gmail.com <32gurihs@gmail.com>
2023-11-23 15:18:06 +0900

    5

--- Nodejs-master/main4.js
+++ Nodejs-master/main4.js
@@ -119,7 +119,7 @@ app.post('/signup', async (req, res) => {
  
         const result = await DB_IO.add_student_table(Student_id, Student_pw, jsonInfo.student_name, jsonInfo.student_number, jsonInfo.department);
         console.log(result);
-        res.redirect('/pages');
+        return res.status(400).json({ message: 'sign up success', status: result });
       } catch (error) {
         console.error('오류 발생:', error);
         res.status(500).send('오류 발생');
