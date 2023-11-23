1b8febc112102fb853d1518388802ddbddd24746
32gurihs@gmail.com <32gurihs@gmail.com>
2023-11-23 23:37:45 +0900

    5

--- Nodejs-master/tester2.js
+++ Nodejs-master/tester2.js
@@ -11,43 +11,44 @@ app.use(passport.initialize());
 // 사용자 인증 로직인 FindUser.findByIdPw를 여기에 추가하세요.
 
 passport.use(new LocalStrategy(
-  {
-    usernameField: 'Student_id',
-    passwordField: 'Student_pw'
-  },
-  function verify(Student_id, Student_pw, cb) {
-    console.log(Student_id, Student_pw)
-    FindUser.findByIdPw(Student_id, Student_pw, function (user) {
-      if (user !== false) return cb(null, user);
-      return cb(null, false, { message: 'no' });
-    });
-  }
+    {
+        usernameField: 'Student_id',
+        passwordField: 'Student_pw'
+    },
+    function verify(Student_id, Student_pw, cb) {
+        console.log(Student_id, Student_pw)
+        FindUser.findByIdPw(Student_id, Student_pw, function (user) {
+            if (user !== false) return cb(null, user);
+            return cb(null, false, { message: 'no' });
+        });
+    }
 ));
 
 // Serialize user information to store in the token
 passport.serializeUser(function (user, done) {
-  done(null, user.Student_id);
+    done(null, user.Student_id);
 });
 
 // Deserialize user information from the token
 passport.deserializeUser(function (Student_id, done) {
-  FindUser.findById(Student_id, function (user) {
-    done(null, user);
-  });
+    FindUser.findById(Student_id, function (user) {
+        done(null, user);
+    });
 });
 
 // Middleware to generate and sign a token after successful login
 function generateToken(req, res, next) {
-  passport.authenticate('local', { session: false }, (err, user, info) => {
-    if (err) { return next(err); }
-    console.log(user);
-    if (!user) {
-      return res.json({ success: false, message: '로그인 실패' });
-    }
+    console.log(Student_id, Student_pw)
+    passport.authenticate('local', { session: false }, (err, user, info) => {
+        if (err) { return next(err); }
+        console.log(user);
+        if (!user) {
+            return res.json({ success: false, message: '로그인 실패' });
+        }
 
-    const token = jwt.sign({ user }, 'your_secret_key', { expiresIn: '1h' });
-    return res.json({ success: true, message: '로그인 성공', token, Student_id: user.Student_id });
-  })(req, res, next);
+        const token = jwt.sign({ user }, 'your_secret_key', { expiresIn: '1h' });
+        return res.json({ success: true, message: '로그인 성공', Student_id: user.Student_id, token });
+    })(req, res, next);
 }
 
 // Route for login
@@ -56,5 +57,5 @@ app.post('/login', generateToken);
 // Your other routes go here
 
 app.listen(1234, () => {
-  console.log('서버가 1234 포트에서 실행 중입니다.');
+    console.log('서버가 1234 포트에서 실행 중입니다.');
 });
