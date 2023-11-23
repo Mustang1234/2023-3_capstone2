4dc4a7fccc052c91deaa87d59beb7f3f967f6a7a
32gurihs@gmail.com <32gurihs@gmail.com>
2023-11-23 23:41:13 +0900

    5

--- Nodejs-master/tester2.js
+++ Nodejs-master/tester2.js
@@ -1,62 +1,80 @@
 const express = require('express');
 const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
+const bodyParser = require('body-parser');
 const jwt = require('jsonwebtoken');
 const FindUser = require('./FindUser.js');
 
 const app = express();
 
-app.use(passport.initialize());
-
-// 사용자 인증 로직인 FindUser.findByIdPw를 여기에 추가하세요.
+// Your existing user authentication logic (FindUser.findByIdPw) goes here
 
 passport.use(new LocalStrategy(
-    {
-        usernameField: 'Student_id',
-        passwordField: 'Student_pw'
-    },
-    function verify(Student_id, Student_pw, cb) {
-        console.log(Student_id, Student_pw)
-        FindUser.findByIdPw(Student_id, Student_pw, function (user) {
-            if (user !== false) return cb(null, user);
-            return cb(null, false, { message: 'no' });
-        });
-    }
+  {
+    usernameField: 'Student_id',
+    passwordField: 'Student_pw'
+  },
+  function verify(Student_id, Student_pw, cb) {
+    FindUser.findByIdPw(Student_id, Student_pw, function (user) {
+      if (user !== false) return cb(null, user);
+      return cb(null, false, { message: 'no' });
+    });
+  }
 ));
 
 // Serialize user information to store in the token
 passport.serializeUser(function (user, done) {
-    done(null, user.Student_id);
+  done(null, user.Student_id);
 });
 
 // Deserialize user information from the token
 passport.deserializeUser(function (Student_id, done) {
-    FindUser.findById(Student_id, function (user) {
-        done(null, user);
-    });
+  FindUser.findById(Student_id, function (user) {
+    done(null, user);
+  });
 });
 
 // Middleware to generate and sign a token after successful login
 function generateToken(req, res, next) {
-    const { Student_id, Student_pw } = req.body;
-    console.log(Student_id, Student_pw)
-    passport.authenticate('local', { session: false }, (err, user, info) => {
-        if (err) { return next(err); }
-        console.log(user);
-        if (!user) {
-            return res.json({ success: false, message: '로그인 실패' });
-        }
+  passport.authenticate('local', { session: false }, (err, user, info) => {
+    if (err) { return next(err); }
+    if (!user) {
+      return res.json({ success: false, message: 'login failed' });
+    }
 
-        const token = jwt.sign({ user }, 'your_secret_key', { expiresIn: '1h' });
-        return res.json({ success: true, message: '로그인 성공', Student_id: user.Student_id, token });
-    })(req, res, next);
+    const token = jwt.sign({ user }, 'your_secret_key', { expiresIn: '1h' });
+    res.json({ success: true, message: 'login success', token });
+  })(req, res, next);
 }
 
 // Route for login
-app.post('/login', generateToken);
+app.post('/login', express.json(), generateToken);
+
+// Middleware to protect routes that require authentication
+function authenticateToken(req, res, next) {
+  const token = req.headers['authorization'];
+
+  if (!token) {
+    return res.sendStatus(401);
+  }
+
+  jwt.verify(token, 'your_secret_key', (err, user) => {
+    if (err) {
+      return res.sendStatus(403);
+    }
+
+    req.user = user;
+    next();
+  });
+}
+
+// Example route that requires authentication
+app.get('/protected', authenticateToken, (req, res) => {
+  res.json({ message: 'Protected route', user: req.user });
+});
 
 // Your other routes go here
 
 app.listen(1234, () => {
-    console.log('서버가 1234 포트에서 실행 중입니다.');
+  console.log('Server is running on port 1234');
 });
