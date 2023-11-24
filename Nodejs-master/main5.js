65da61b47d0c1d499d41404f560bc41dfd98bf0e
32gurihs@gmail.com <32gurihs@gmail.com>
2023-11-24 14:55:00 +0900

    5

--- Nodejs-master/main5.js
+++ Nodejs-master/main5.js
@@ -323,7 +323,7 @@ app.post('/page_delete_process', authenticateToken, function (req, res) {
 app.get('/main_page', authenticateToken, async (req, res) => {
   try {
     const Student_id = req.user.Student_id;
-    console.log(Student_id)
+    console.log(req.user)
     const year_semester = req.query.year_semester;
     var returnJson = { Student_id: Student_id, retCode: false, Student_name: '', Student_number: '',
       department: '', Speed: 0, timeTable: [], schedule: [], ProfilePhoto: null
