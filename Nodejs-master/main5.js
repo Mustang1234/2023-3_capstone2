1273a498a721593c4b1dc6b88c4bff209a04855a
32gurihs@gmail.com <32gurihs@gmail.com>
2023-11-24 19:19:51 +0900

    5

--- Nodejs-master/main5.js
+++ Nodejs-master/main5.js
@@ -421,6 +421,7 @@ app.post('/get_timetable_from_portal', authenticateToken, async (req, res) => {
       i = i + 1;
       try {
         jsonInfo = JSON.parse(await Eclass.Eclass(Student_id, portal_id, portal_pw));
+        console.log(jsonInfo);
         if (jsonInfo.timeTable.length !== 0) break;
         if (jsonInfo.retCode === false) {
           res.json({ returnCode: "portal_login_failed" });
