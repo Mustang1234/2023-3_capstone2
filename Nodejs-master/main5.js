c84e58d91b191b07180451cd63f363aff5209731
32gurihs@gmail.com <32gurihs@gmail.com>
2023-11-29 17:15:57 +0900

    4

--- Nodejs-master/main5.js
+++ Nodejs-master/main5.js
@@ -376,6 +376,7 @@ app.get('/my_page', authenticateToken, async (req, res) => {
     returnJson.department = student_info.department;
     returnJson.Speed = student_info.Speed;
     const bufferData = JSON.parse(await DB_IO.get_student_photo_table(Student_id)).ProfilePhoto;
+    console.log(bufferData);
     const student_photo_info = bufferData.toString('base64');
     console.log(student_photo_info);
     returnJson.ProfilePhoto = student_photo_info;
