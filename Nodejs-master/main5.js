c191d0c2c4e69f4896cdfb9c8b784ba856ffa430
32gurihs@gmail.com <32gurihs@gmail.com>
2023-11-29 20:01:12 +0900

    4

--- Nodejs-master/main5.js
+++ Nodejs-master/main5.js
@@ -377,7 +377,7 @@ app.get('/my_page', authenticateToken, async (req, res) => {
     returnJson.Speed = student_info.Speed;
     //const bufferData = Buffer.from(JSON.parse(await DB_IO.get_student_photo_table(Student_id)).ProfilePhoto.data);
     //const student_photo_info = bufferData.toString('base64');
-    returnJson.ProfilePhoto = JSON.parse(await DB_IO.get_student_photo_table(Student_id)).ProfilePhoto.data;
+    returnJson.ProfilePhoto = JSON.parse(await DB_IO.get_student_photo_table(Student_id)).ProfilePhoto;
     returnJson.retCode = true;
     res.status(200).json(returnJson);
   } catch (error) {
@@ -389,8 +389,8 @@ app.get('/my_page', authenticateToken, async (req, res) => {
 app.post('/my_page_photo_upload', authenticateToken, async (req, res) => {
   try {
     const Student_id = req.user.user.Student_id;
-    const ProfilePhoto = req.body.ProfilePhoto;
-    //const ProfilePhoto = fs.readFileSync('hello.jpg');
+    //const ProfilePhoto = req.body.ProfilePhoto;
+    const ProfilePhoto = fs.readFileSync('hello.jpg');
     const result = await DB_IO.update_photo_student_table(Student_id, ProfilePhoto);
     res.status(200).json({ Student_id: Student_id, success: result, ProfilePhoto: ProfilePhoto });
   } catch (error) {
