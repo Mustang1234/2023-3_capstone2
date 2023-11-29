f832b2c53bd990dd2b3f88eb90a69a82e3ac8899
32gurihs@gmail.com <32gurihs@gmail.com>
2023-11-29 17:00:15 +0900

    4

--- Nodejs-master/main5.js
+++ Nodejs-master/main5.js
@@ -389,6 +389,8 @@ app.post('/my_page_photo_upload', authenticateToken, async (req, res) => {
   try {
     const Student_id = req.user.user.Student_id;
     const ProfilePhoto = req.body.ProfilePhoto;
+    const base64Image = ProfilePhoto.toString('base64');
+    console.log(base64Image);
     //const ProfilePhoto2 = fs.readFileSync('hello.jpg');
     //console.log(ProfilePhoto2);
     const result = await DB_IO.update_photo_student_table(Student_id, ProfilePhoto);
