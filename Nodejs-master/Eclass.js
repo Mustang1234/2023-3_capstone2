23b44351c5ff0d35acbf0f7cb4163f3b6543883f
32gurihs@gmail.com <32gurihs@gmail.com>
2023-12-03 00:51:51 +0900

    4

--- Nodejs-master/Eclass.js
+++ Nodejs-master/Eclass.js
@@ -98,8 +98,6 @@ async function Eclass(_studentID, portal_id, portal_pw) {
     }
 
     const pageContent1 = await page.content();
-
-    console.log(pageContent1)
     
     const wow5 = '성명';
     pageContent_name = pageContent1.substring(pageContent1.indexOf(wow5), pageContent1.indexOf(wow5)+200);
