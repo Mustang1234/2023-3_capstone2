dec982204f8c55200f31a9ad07c2ad941f18d3d1
32gurihs@gmail.com <32gurihs@gmail.com>
2023-12-02 19:27:42 +0900

    4

--- Nodejs-master/Eclass.js
+++ Nodejs-master/Eclass.js
@@ -87,7 +87,7 @@ async function Eclass(_studentID, portal_id, portal_pw) {
     //await page.setCookie(...cookies);
     //await page.waitForNavigation();
     try {
-        await page.waitForNavigation({ timeout : 1000 });
+        await page.waitForNavigation({ timeout : 2500 });
     } catch (error) {
         if (error instanceof puppeteer.errors.TimeoutError) {
             //console.error('Navigation timed out');
@@ -148,7 +148,7 @@ async function Eclass(_studentID, portal_id, portal_pw) {
     //await page2.setCookie(...cookies);
     //await page2.waitForNavigation();
     try {
-        await page2.waitForNavigation({ timeout: 1000 });
+        await page2.waitForNavigation({ timeout: 2500 });
     } catch (error) {
         if (error instanceof puppeteer.errors.TimeoutError) {
             //console.error('Navigation timed out');
