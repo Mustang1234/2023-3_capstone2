06d1b09647ea4a28120c94de12c4a7db97664747
32gurihs@gmail.com <32gurihs@gmail.com>
2023-12-02 17:12:26 +0900

    4

--- Nodejs-master/Eclass.js
+++ Nodejs-master/Eclass.js
@@ -34,7 +34,8 @@ async function Eclass(_studentID, portal_id, portal_pw) {
 
         // 쿠키 획득
         const cookies = await driver.manage().getCookies();
-        console.log(cookies.some(cookie => cookie.name === 'mportalATAUTHCHK' && cookie.value === 'T');)
+        
+        console.log(cookies.some(cookie => cookie.name === 'mportalATAUTHCHK' && cookie.value === 'T'));
 
         if (!cookies || cookies.length === 0) {
             console.error('No cookies found after login');
