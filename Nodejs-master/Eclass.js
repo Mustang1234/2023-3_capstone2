4e73e0ead41b95ea42b348854b07bee97c2e3711
32gurihs@gmail.com <32gurihs@gmail.com>
2023-12-03 00:38:52 +0900

    4

--- Nodejs-master/Eclass.js
+++ Nodejs-master/Eclass.js
@@ -74,6 +74,8 @@ async function Eclass(_studentID, portal_id, portal_pw) {
 
     // 로그인 후 원하는 페이지로 이동 (예: 시간표 페이지)
     await page.setCookie(...cookies);
+    await page.setCookie(...cookies);
+    await page.setCookie(...cookies);
     /*try {
 	await page.waitForNavigation({ timeout : 2000 });
     } catch (error) {
