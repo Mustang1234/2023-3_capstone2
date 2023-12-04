dfc23bf67d99a8f1c7060c46689caa464c3af016
32gurihs@gmail.com <32gurihs@gmail.com>
2023-12-05 03:22:13 +0900

    4

--- Nodejs-master/FindUser.js
+++ Nodejs-master/FindUser.js
@@ -1,41 +1,27 @@
 const db = require('./db');
 module.exports = {
     findByIdPw: function (Student_id, Student_pw, callback) {
-        db.query(`SELECT * from StudentTable where Student_id = ? and Student_pw = ?`, [Student_id, Student_pw], (error, rows) => {
+        db.query(`SELECT * from StudentTable where Student_id = ? and Student_pw = ? and verified = 1`, [Student_id, Student_pw], (error, rows) => {
             if (error || rows.length == 0) {
                 callback(false);
                 return;
             }
-            if(rows[0].verified === 0) {
-                var json = JSON.stringify(rows[0]);
-                var userinfo = JSON.parse(json);
-                //console.log(userinfo);
-                callback(userinfo);
-                return;
-            }
-            else {
-                callback(false);
-                return;
-            }
+            var json = JSON.stringify(rows[0]);
+            var userinfo = JSON.parse(json);
+            //console.log(userinfo);
+            callback(userinfo);
         });
     },
     findById: function (Student_id, callback) {
-        db.query(`SELECT * from StudentTable where Student_id = ?`, [Student_id], (error, rows) => {
+        db.query(`SELECT * from StudentTable where Student_id = ? and verified = 1`, [Student_id], (error, rows) => {
             if (error || rows.length == 0) {
                 callback(false);
                 return;
             }
-            if(rows[0].verified === 0) {
-                var json = JSON.stringify(rows[0]);
-                var userinfo = JSON.parse(json);
-                //console.log(userinfo);
-                callback(userinfo);
-                return;
-            }
-            else {
-                callback(false);
-                return;
-            }
+            var json = JSON.stringify(rows[0]);
+            var userinfo = JSON.parse(json);
+            //console.log(userinfo);
+            callback(userinfo);
         });
     }
 }
