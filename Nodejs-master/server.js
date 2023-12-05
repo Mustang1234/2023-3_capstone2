704a7a8d0ecc9d047d2c7ee84766035a05a7a8ca
32gurihs@gmail.com <32gurihs@gmail.com>
2023-12-05 20:40:51 +0900

    4

--- Nodejs-master/server.js
+++ Nodejs-master/server.js
@@ -6,14 +6,14 @@ const app = express();
 const server = http.createServer(app);
 const io = socketIO(server);
 
-const mysql = require('mysql2');
+/*const mysql = require('mysql2');
 const db = mysql.createConnection({
 	host : 'localhost',
 	user : 'root',
 	password : '1234',
 	database : 'my_db'
 });
-db.connect();
+db.connect();*/
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/chat_index.html');
@@ -30,7 +30,7 @@ io.on('connection', (socket) => {
         console.log('hi');
         
         // 데이터베이스에서 채팅 기록 가져오기
-        db.query(
+        /*db.query(
             `SELECT sender_name, message FROM chat_messages WHERE room = ? ORDER BY id`,
             [room],
             (err, results) => {
@@ -46,7 +46,7 @@ io.on('connection', (socket) => {
                 // 다른 사용자들에게 새로운 사용자 입장을 알림
                 socket.to(room).emit('chat message', '시스템', `${userId} ${userName} 님이 방에 참가하셨습니다.`);
             }
-        );
+        );*/
         
         // Notify other users in the room about the new user
         socket.to(room).emit('chat message', 'System', `${userId} ${userName} has joined the room`);
@@ -54,11 +54,11 @@ io.on('connection', (socket) => {
         // Handle chat messages in the room
         socket.on('chat message', (msg) => {
             // Save the message to the database
-            db.query(`INSERT INTO chat_messages (sender_id, sender_name, room, message) VALUES (?, ?, ?, ?)`,
-             [userId, userName, room, msg], (err) => {
-                if (err) throw err;
-                console.log('Message saved to database');
-            });
+            //db.query(`INSERT INTO chat_messages (sender_id, sender_name, room, message) VALUES (?, ?, ?, ?)`,
+            // [userId, userName, room, msg], (err) => {
+            //    if (err) throw err;
+            //    console.log('Message saved to database');
+            //});
 
             // Send the message to the specific room
             io.to(room).emit('chat message', userName, msg);
