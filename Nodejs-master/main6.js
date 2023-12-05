55ed0486f060ab92e87ff9ae69fc68a7f0519c51
32gurihs@gmail.com <32gurihs@gmail.com>
2023-12-05 19:58:02 +0900

    4

--- Nodejs-master/main6.js
+++ Nodejs-master/main6.js
@@ -8,41 +8,11 @@ const sanitizeHtml = require('sanitize-html');
 const port = 1234;
 //const ip = '20.39.186.138';
 const bodyParser = require('body-parser');
-//var swaggerJsdoc = require("swagger-jsdoc");
-//var swaggerUi = require("swagger-ui-express");
 
-/*const options = {
-  definition: {
-    openapi: "4.0.0",
-    info: {
-      title: "LogRocket Express API with Swagger",
-      version: "0.1.0",
-      description:
-        "This is a simple CRUD API application made with Express and documented with Swagger",
-      license: {
-        name: "MIT",
-        url: "https://spdx.org/licenses/MIT.html",
-      },
-    },
-    servers: [
-      {
-        url: "http://20.39.186.138:1234",
-      },
-    ],
-  },
-  apis: ["./Eclass.js", "./db_id.js"],
-};
-const specs = swaggerJsdoc(options);
-app.use("/api-docs",
-  swaggerUi.serve,
-  swaggerUi.setup(specs)
-);*/
 
 const compression = require('compression');
 const helmet = require('helmet');
-//const cookie = require('cookie');
-//const session = require('express-session')
-//const FileStore = require('session-file-store')(session)
+
 const jwt = require('jsonwebtoken');
 const passport = require('passport')
   , LocalStrategy = require('passport-local')
@@ -136,7 +106,7 @@ function authenticateToken(req, res, next) {
   const token = req.headers['authorization'];
 
   if (!token) {
-    return res.send('Unauthorized');;
+    return res.status(401).json('Unauthorized');
   }
 
   jwt.verify(token, token_secret_key.token_secret_key, (err, user) => {
