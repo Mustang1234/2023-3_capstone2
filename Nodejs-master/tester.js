6709c4ae07a02473afd2359c3c736e2fb4b9a88e
32gurihs@gmail.com <32gurihs@gmail.com>
2023-12-05 20:03:05 +0900

    4

--- Nodejs-master/tester.js
+++ Nodejs-master/tester.js
@@ -1,5 +1,11 @@
+const express = require('express');
+const swaggerUi = require('swagger-ui-express');
 const swaggerAutogen = require('swagger-autogen')({ language: 'ko' });
 
+const app = express();
+const PORT = 1235;
+
+// Swagger 자동 생성 설정
 const doc = {
   info: {
     title: "swagger_api",
@@ -10,9 +16,15 @@ const doc = {
   // schemes: ["https" ,"http"],
 };
 
-const outputFile = "./swagger-output.json";	// 같은 위치에 swagger-output.json을 만든다.
-const endpointsFiles = [
-  "./main6.js"					// 라우터가 명시된 곳을 지정해준다.
-];
+const outputFile = "./swagger-output.json";
+const endpointsFiles = ["./main6.js", './db_io.js', './Eclass.js', './FindUser.js'];
 
 swaggerAutogen(outputFile, endpointsFiles, doc);
+
+// Swagger 미들웨어 추가
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(require('./swagger-output.json')));
+
+// 서버 시작
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
