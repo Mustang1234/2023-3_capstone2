2b3eec7af052d59e1a4b66de2b9a50fb17c58e00
32gurihs@gmail.com <32gurihs@gmail.com>
2023-12-06 22:45:23 +0900

    4

--- Nodejs-master/main6.js
+++ Nodejs-master/main6.js
@@ -553,7 +553,7 @@ app.get('/list_whole_team', authenticateToken, async (req, res) => {
     const search = req.query.search;
     const year_semester = _year_semester();
     const result = JSON.parse(await DB_IO.list_whole_team(Student_id, year_semester));
-    //console.log(result)
+    console.log(result)
     
     const jsonInfo = { teams: [] }
     for (let i = 0; i < result.length; i++) {
